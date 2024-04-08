2e7a4be9bf614b1dce878c4787be7da29083e941
akhileshkumar07 <akhilesh.kumar070197@gmail.com>
2024-04-08 19:33:08 +0530

    Added few more arguments

--- Part_A/argeparser.py
+++ Part_A/argeparser.py
@@ -8,8 +8,11 @@ def arg_parser():
     parser.add_argument("--filter_org","-fo", type=int, default=1, help="filter organization(multiplier)")
     parser.add_argument("--activation","-ac", type=str, default="ReLU", choices=["ReLU", "GELU", "SiLU", "Mish"], help="activation function")
     parser.add_argument("--dense_size","-dsz", type=int, default=256, help="size of dense layer")
-    parser.add_argument("--dropout","-do",  type=float, default=0.2, help="dropout rate")
+    parser.add_argument("--dropout","-drp",  type=float, default=0.2, help="dropout rate")
     parser.add_argument("--use_batch_norm","-bn",action="store_true", help="use batch normalization")
+    parser.add_argument("--num_epochs","-e", type=int , default=5, help="number of epochs")
+    parser.add_argument("--learning_rate","-lr", type=float, default=0.001, help="learning rate for the optimizer")
+
     return parser
 
 def get_activation(activation):
