e90bb4b5c1197309be540016c476c0a954f0493d
akhileshkumar07 <akhilesh.kumar070197@gmail.com>
2024-04-08 19:33:37 +0530

    Minor changes

--- Part_A/dataloader.py
+++ Part_A/dataloader.py
@@ -2,7 +2,7 @@ import os
 import torch
 import torchvision.transforms as transforms
 from torchvision import datasets
-from torch.utils.data import random_split
+from torch.utils.data import random_split, DataLoader
 
 IMAGE_DIM = 224
 BATCH_SIZE = 64
@@ -16,7 +16,6 @@ def load_dataset(DATA_DIR="inaturalist_12K", augment_data=False):
             transforms.RandomHorizontalFlip(),
             transforms.RandomVerticalFlip(),
             transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
-            transforms.RandomResizedCrop(IMAGE_DIM, scale=(0.8, 1.0)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
@@ -43,8 +42,8 @@ def load_dataset(DATA_DIR="inaturalist_12K", augment_data=False):
 
     test_dataset = datasets.ImageFolder(test_dir, transform=test_transform)
 
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
     return train_loader, val_loader, test_loader
